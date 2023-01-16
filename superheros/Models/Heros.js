8c9ad7ef1bfd8f0cc63fa4d3bc78c114d1d23170
Rohtash Talan <rohtashtalancompany@gmail.com>
2023-01-16 11:21:40 +0530

    schema created

---
+++ superheros/Models/Heros.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+
+
+const HeroSchema = () => mongoose.Schema({
+    superHero: {
+        type: String,
+        required: [true, 'Please enter SuperHero name'],
+        unique:true
+    },
+
+    realName: {
+        type: String,
+        required: true,
+        maxlength: [200, 'Please keep real name short']
+    }
+})
+
+
+exports.module = mongoose.model.Hero || mongoose.model('Hero', HeroSchema)
