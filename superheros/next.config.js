d3908e94b92ca5a1fe496c53e283a9c71da2e9c2
Rohtash Talan <rohtashtalancompany@gmail.com>
2023-01-18 22:41:58 +0530

    super app deleted

--- superheros/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    MONGO_URI: "mongodb://localhost:27017/SUPERHERO_NEXTJS_APP"
-  }
-}
-
-module.exports = nextConfig
